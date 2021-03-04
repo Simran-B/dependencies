309834002f96cee3ebb2de646c1b8bc83f8297cb
itsmattkc <itsmattkc@gmail.com>
2021-03-04 11:28:36 +1100

    script inside yaml

--- opencolorio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-git clone https://github.com/AcademySoftwareFoundation/OpenColorIO.git
-cd OpenColorIO
-git checkout v2.0.0
-mkdir build
-cd build
-cmake .. -G Ninja -DOCIO_BUILD_PYTHON=OFF -DOCIO_BUILD_APPS=OFF -DOCIO_BUILD_TESTS=OFF -DOCIO_BUILD_GPU_TESTS=OFF -DOCIO_BUILD_DOCS=OFF
-ninja
